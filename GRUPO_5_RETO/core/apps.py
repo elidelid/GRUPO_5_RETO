efc2be3d6588b733bb6b90f94104c3c1f4eb72de
elidelid <elinamagua@est.itsgg.edu.ec>
2020-07-28 22:36:08 -0500

    Initial commit

---
+++ GRUPO_5_RETO/core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    name = 'GRUPO_5_RETO.core'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\PycharmProjects\GRUPO_5_RETO\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
